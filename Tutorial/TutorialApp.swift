9894d0cf1616e07e6a2774963b9379cdd710513b
Jhones Michael <82251374+jhoneshark@users.noreply.github.com>
2024-07-30 00:51:58 -0300

    Initial Commit

---
+++ Tutorial/TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialApp.swift
+//  Tutorial
+//
+//  Created by Jhones on 30/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
