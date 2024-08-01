b5c5639dc3891d663367ad0e72e6ddbd61a35618
Jhones Michael <82251374+jhoneshark@users.noreply.github.com>
2024-08-01 02:55:41 -0300

    Refactor ContentView to use HStack and Card view for displaying cards

--- Tutorial/ContentView.swift
+++ Tutorial/ContentView.swift
@@ -7,17 +7,58 @@
 
 import SwiftUI
 
+//posso usar assim VStack(alignment: .leading, spacing: 20)
+//mudando de VStack para Zstack
+// borda pontilhada   .strokeBorder(style: StrokeStyle(lineWidth: 10, dash: [10,2]) )
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            Card(isFaceUp: true)
+            Card(isFaceUp: false)
+            Card(isFaceUp: true)
         }
+        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
         .padding()
     }
 }
+
+struct Card: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        ZStack(content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12).foregroundColor(.gray)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 10)
+                Text("💻").padding().font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12).foregroundColor(.blue)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 10)
+                Text("").padding().font(.largeTitle)
+            }
+        })
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
